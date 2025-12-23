6a5fc6442b5dcb9b010bd717eab693276dc07362
ryanhenrqq <ryanh8116@gmail.com>
2025-12-22 22:02:11 -0300

    chore: added window title

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import tkinter as tk
 
 # Parametros da Janela
 windowTitle = "OoniquePass - Gerador de senhas"
-windowWidth = 500
-windowHeight = 400
+windowWidth = 1200
+windowHeight = 800
 
 root = tk.Tk()
 root.title(windowTitle)
 root.geometry(f"{windowWidth}x{windowHeight}")
 root.resizable(0,0)
+
+headerTitle = tk.Label(root, text=windowTitle, font=("System", 28, "bold"))
+headerTitle.pack()
 
 root.mainloop()
