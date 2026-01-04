d459ecec07a4b294a355cac420a37bd045bc73ef
ryanhenrqq <ryanh8116@gmail.com>
2026-01-04 00:13:56 -0300

    feat: basic function of the string generator is now working fine

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import tkinter as tk
+import random
 from tkinter import messagebox
+
+password = []
+stripOut = ""
+numbLength = 6
 
 # Parametros da Janela
 windowTitle = "OoniquePass - Gerador de senhas"
@@ -11,12 +16,45 @@ generatedPassword = "Clique em 'Gerar!' para gerar uma senha"
 
 # Componentes
 def handleGerar():
-    exceptMainentance()
+    global password
+    global numbLength
+    global generatedPassword
+    global stripOut
+    try:
+        cha = int(characterAmount.get())
+    except ValueError:
+        exceptValueError()
+        return
+    numbLength = cha
+    print(cha)
+    password = []
+    stripOut = ""
 
+    a = 0
+    while a < numbLength:
+        string = "abcdefghijklmnopqrstuvwxyz"
+        print(random.choice(string))
+        password.append(random.choice(string))
+        a = a+1
+    
+    b = 0
+    bx = ""
+    while b < numbLength:
+        output = f"{bx}{password[b]}"
+        bx = f"{output}"
+        b = b+1
+    print("Password: ",bx, "\n If you seeing this, the code has worked as well!")
+    stripOut = str(bx)
+    passOutput["text"] = stripOut
 
 def exceptMainentance():
     mainentanceMessage = "Oops! O item que você executou esta em manutenção, tente novamente mais tarde."
     messagebox.showinfo(windowTitle, mainentanceMessage)
+
+
+def exceptValueError():
+    errorMessage = "Oops! Você precisa digitar um numero válido, tente novamente."
+    messagebox.showerror(windowTitle, errorMessage)
 
 # Call inicial do tk
 root = tk.Tk()
